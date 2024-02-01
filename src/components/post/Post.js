3b7a5f25de2a02670f82ebb8b6ad3ab172db04b7
Akash <kumawat.aakash515@gmail.com>
2024-02-01 15:03:11 +0530

    post component done

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,36 +1,50 @@
 import React from "react";
 import "./post.css";
 import { MoreVert } from "@mui/icons-material";
-function Post() {
+function Post({post}) {
+    console.log(post);
   return (
     <div className="post">
       <div className="postWrapper">
         <div className="postTop">
           <div className="postTopLeft">
-            <img className="postProfileImg" src={require('../Images/Person/PR1.jpeg')} alt="" />
+            <img
+              className="postProfileImg"
+              src={require("../Images/Person/PR1.jpeg")}
+              alt=""
+            />
             <span className="postUserName">Mehar Sharma </span>
-            <span className="postDate"> 5 min ago</span>
-
-
-
+            <span className="postDate"> {post.date}</span>
           </div>
           <div className="postTopRight">
-            <MoreVert/>
+            <MoreVert />
           </div>
         </div>
-        <div className="PostCenter">
-            <span className="postText">Hey! Its My Fist post:) </span>
-            <img className="postImg" src={require ('../Images/post/1.jpeg')} alt="" />
+        <div className="postCenter">
+          <span className="postText">{post?.desc} </span>
+          <img
+            className="postImg"
+            src={post.photo}           alt=""
+          />
+        
         </div>
         <div className="postBottom">
-            <div className="postBottomLeft">
-                <img className="likeIcon" src={require ('../Images/like.png')} alt="" />
-                <img className="likeIcon" src={require ('../Images/heart.png')} alt="" />
-                <span className="postLikeCounter">32 People Like It</span>
-            </div>
-            <div className="postBottomRight">
-                <span className="postCommentText">9 Comments</span>
-            </div>
+          <div className="postBottomLeft">
+            <img
+              className="likeIcon"
+              src={require("../Images/like.png")}
+              alt=""
+            />
+            <img
+              className="likeIcon"
+              src={require("../Images/heart.png")}
+              alt=""
+            />
+            <span className="postLikeCounter">{post.like}</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">{post.comment}</span>
+          </div>
         </div>
       </div>
     </div>
--- src/components/rightbar/Rightbar.js
+++ src/components/rightbar/Rightbar.js
@@ -1,11 +1,147 @@
-import React from 'react'
-import  './rightbar.css'
+import React from "react";
+import "./rightbar.css";
 function Rightbar() {
   return (
-    <div className='rightbar'>
-      Rightbar
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img
+            className="birthdayImg"
+            src={require("../Images/gift.png")}
+            alt=""
+          />
+          <span className="birthdayText">
+            <b>Ajay Gehnoliya</b> and <b>Other Friends</b> have a birthday Today
+          </span>
+        </div>
+        <img className="rightbarAd" src={require("../Images/ad.png")} alt="" />
+        <h4 className="rightbarTitle"> Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src={require('../Images/Person/3.jpeg')}
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUserName">John Fizale</span>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Rightbar
+export default Rightbar;
